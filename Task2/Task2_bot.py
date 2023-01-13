ad718a859e78b5ce1dfc8ed6573b4f96f073c1c4
Oksana Blokhina <oksa-blokhi@yandex.ru>
2023-01-13 20:53:15 +0300

    Task2

---
+++ Task2/Task2_bot.py
@@ -0,0 +1,14 @@
+from random import randint
+
+n=2021
+Flag=0
+while n>0:
+    a=int(input('Игрок 1 введите число конфет от 1 до 28 включительно'))
+    n=n-a
+    Flag=1
+    if n>0:
+        b=randint(1,28)
+        n=n-b
+        Flag=2
+print(f'Победил игрок {Flag}') 
+
