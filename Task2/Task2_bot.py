d79cd5fcecdc92a755ed551815fbda5489a30167
Oksana Blokhina <oksa-blokhi@yandex.ru>
2023-01-13 20:56:59 +0300

    Task2

--- Task2/Task2_bot.py
+++ Task2/Task2_bot.py
@@ -1,13 +1,18 @@
 from random import randint
 
-n=2021
+n=201
 Flag=0
 while n>0:
-    a=int(input('Игрок 1 введите число конфет от 1 до 28 включительно'))
+    # a=int(input('Игрок 1 введите число конфет от 1 до 28 включительно'))
+    a=int(input('Игрок 2 введите число конфет от 1 до 28 включительно'))
+    if a>28 or a<1:
+        print('Вы ввели некорректное число, введите правильное число')
+        continue
     n=n-a
     Flag=1
     if n>0:
         b=randint(1,28)
+        print(b)
         n=n-b
         Flag=2
 print(f'Победил игрок {Flag}') 
