67d254065e188548d2d6bc882c51d2a6fc353783
Oksana Blokhina <oksa-blokhi@yandex.ru>
2023-01-15 14:30:57 +0300

    Task4_new

--- Task4/Task4.py
+++ Task4/Task4.py
@@ -12,16 +12,31 @@ def coding_zip(str1):
         i+=1
         count=1  
     return zip_str 
-str1 = input('Введите строку для кодировки: ')
+# str1 = input('Введите строку для кодировки: ')
+
+str1='/Users/denisblohin/Desktop/Обучение/Python/HWork/HW5_1/Task4/Text2.txt'
+with open(str1, 'r', encoding='UTF-8') as file:
+    str1=file.readline()
+print(str1)
 str2=coding_zip(str1)
 print(str2)
 
+name='/Users/denisblohin/Desktop/Обучение/Python/HWork/HW5_1/Task4/Text3.txt'
+
+with open(name, 'w', encoding='UTF-8') as file2:
+    file2.write(str2)
+
+
+
 def decoding_zip(str1):
-    dezip_str=''
-    i=0
-    while i<len(str1):
-        dezip_str+=int(str1[i])*str1[i+1]
-        i+=2
-    return dezip_str
+    str1='/Users/denisblohin/Desktop/Обучение/Python/HWork/HW5_1/Task4/Text3.txt'
+    with open(str1, 'r', encoding='UTF-8') as file:
+        str1=file.readline()
+        dezip_str=''
+        i=0
+        while i<len(str1):
+            dezip_str+=int(str1[i])*str1[i+1]
+            i+=2
+        return dezip_str
 
 print(decoding_zip(str2))
